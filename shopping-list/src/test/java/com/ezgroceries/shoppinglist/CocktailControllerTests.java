67c2d1b04d79f31f5f6d94a6bf55548f9fd2c8a2
Bart Van Rillaer <bart.vanrillaer@kbc.be>
2019-09-30 12:35:33 +0200

    add first test for cocktail controller

---
+++ shopping-list/src/test/java/com/ezgroceries/shoppinglist/CocktailControllerTests.java
@@ -0,0 +1,33 @@
+package com.ezgroceries.shoppinglist;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.ezgroceries.shoppinglist.controller.CocktailController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CocktailController.class)
+public class CocktailControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void searchForCocktails() throws Exception
+    {
+        mvc.perform(MockMvcRequestBuilders
+                .get("/cocktails?search=Russian")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+    }
+
+}
