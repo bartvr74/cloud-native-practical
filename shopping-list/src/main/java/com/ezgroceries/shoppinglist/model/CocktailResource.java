a3787738c079ab86c357c3d32d3823f28bb74f0a
Bart Van Rillaer <bart.vanrillaer@kbc.be>
2019-09-30 13:14:49 +0200

    minor cleanup

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/model/CocktailResource.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/model/CocktailResource.java
@@ -12,14 +12,14 @@ public class CocktailResource {
     private String url;
     private List<String> ingredients;
 
-    public CocktailResource(UUID id, String description, String typeOfGlass, String instruction, String url,
-            List<String> ingredients) {
+    public CocktailResource(UUID id, String description, String typeOfGlass,
+            String instruction, String url, List<String> ingredients) {
         this.id = id;
         this.description = description;
         this.typeOfGlass = typeOfGlass;
         this.instruction = instruction;
-        this.url = url;
         this.ingredients = ingredients;
+        this.url = url;
     }
 
     public UUID getId() {
