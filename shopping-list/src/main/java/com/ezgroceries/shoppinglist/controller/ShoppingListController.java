3680e6d5347a6ee2ebcb357ec7ee66c63ef2f387
Bart Van Rillaer <bart.vanrillaer@kbc.be>
2019-10-04 12:11:29 +0200

    refactoring shopping list to use service

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/ShoppingListController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/controller/ShoppingListController.java
@@ -1,11 +1,12 @@
 package com.ezgroceries.shoppinglist.controller;
 
-import com.ezgroceries.shoppinglist.contract.CreateShoppingList;
 import com.ezgroceries.shoppinglist.contract.CocktailReference;
+import com.ezgroceries.shoppinglist.contract.CreateShoppingList;
 import com.ezgroceries.shoppinglist.contract.ShoppingListResource;
-import java.util.Arrays;
+import com.ezgroceries.shoppinglist.service.ShoppingListService;
 import java.util.List;
 import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,61 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(produces = "application/json")
 public class ShoppingListController {
 
-    // todo dummy data for mobile team
-    // todo replace with services / repositories later on similar to cocktail controller
+    private ShoppingListService shoppingListService;
+
+    @Autowired
+    public ShoppingListController(ShoppingListService shoppingListService) {
+        this.shoppingListService = shoppingListService;
+    }
 
     @PostMapping("/shopping-lists")
-    public ResponseEntity<ShoppingListResource> createShoppingList(
-            @RequestBody CreateShoppingList createShoppingList) {
-        ShoppingListResource newShoppingList = newShoppingList(createShoppingList);
+    public ResponseEntity<ShoppingListResource> createShoppingList(@RequestBody CreateShoppingList createShoppingList) {
+        ShoppingListResource newShoppingList = shoppingListService.create(createShoppingList.getName());
         return new ResponseEntity<>(newShoppingList, HttpStatus.CREATED);
     }
 
     @PostMapping("/shopping-lists/{shoppingListId}/cocktails")
-    public ResponseEntity<List<CocktailReference>> addCocktailReferences(
-            @PathVariable(name = "shoppingListId") UUID shoppingListId,
-            @RequestBody List<CocktailReference> cocktailReferences) {
+    public ResponseEntity<List<CocktailReference>> addCocktailReferences(@PathVariable(name = "shoppingListId")
+            UUID shoppingListId, @RequestBody List<CocktailReference> cocktailReferences) {
         List<CocktailReference> addedCocktailReferences =
-                addCocktailReferencesToShoppingList(shoppingListId, cocktailReferences);
+                shoppingListService.addCocktails(shoppingListId, cocktailReferences);
         return new ResponseEntity<>(addedCocktailReferences, HttpStatus.OK);
     }
 
     @GetMapping("/shopping-lists/{shoppingListId}")
     public ShoppingListResource findShoppingList(@PathVariable(name = "shoppingListId") UUID shoppingListId) {
-        return getShoppingList(shoppingListId);
+        return shoppingListService.get(shoppingListId);
     }
 
     @GetMapping("/shopping-lists")
     public List<ShoppingListResource> getShoppingLists() {
-        return getAllShoppingList();
-    }
-
-    private ShoppingListResource newShoppingList(CreateShoppingList shoppingListInput) {
-        return new ShoppingListResource(
-                UUID.randomUUID(),
-                shoppingListInput.getName()
-        );
-    }
-
-    private ShoppingListResource getShoppingList(UUID shoppingListId) {
-        return new ShoppingListResource(
-                UUID.fromString(shoppingListId.toString()), "My shop list",
-                Arrays.asList("Tequila", "Triple sec", "Lime juice", "Salt"));
-    }
-
-    private List<CocktailReference> addCocktailReferencesToShoppingList(UUID shoppingListId,
-            List<CocktailReference> cocktailReferences) {
-        CocktailReference addedCocktail = new CocktailReference();
-        addedCocktail.setCocktailId(cocktailReferences.get(0).getCocktailId());
-        return Arrays.asList(addedCocktail);
-    }
-
-    private List<ShoppingListResource> getAllShoppingList() {
-        return Arrays.asList(
-                getShoppingList(UUID.randomUUID()),
-                getShoppingList(UUID.randomUUID()),
-                getShoppingList(UUID.randomUUID())
-        );
+        return shoppingListService.lists();
     }
 
 }
